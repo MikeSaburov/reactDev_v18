00e9adc38e95e7b63c4a1549da5b85aa36996b71
Mikhail Saburov <saburov1985@gmail.com>
2024-04-23 23:11:37 +1100

    добавить компонент profile

--- app/page.js
+++ app/page.js
@@ -2,13 +2,6 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        src={'https://i.imgur.com/MK3eW3Am.jpg'}
-        alt="Кэтрин Джонсон"
-        width={150}
-        height={150}
-      />
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
---
+++ components/profile.jsx
@@ -0,0 +1,12 @@
+'use client';
+
+export const Profile = () => {
+  return (
+    <Image
+      src={'https://i.imgur.com/MK3eW3Am.jpg'}
+      alt="Кэтрин Джонсон"
+      width={150}
+      height={150}
+    />
+  );
+};
