9e47d0555fbdcd280f88bde2f21d7dcdd69608fa
Mikhail Saburov <saburov1985@gmail.com>
2024-04-23 23:15:07 +1100

    добавить компонент gallery

---
+++ components/gallery.jsx
@@ -0,0 +1,12 @@
+import { Profile } from '@/components/profile';
+
+export const Gallery = () => {
+  return (
+    <div>
+      <h1>Список замечательный ученых</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </div>
+  );
+};
