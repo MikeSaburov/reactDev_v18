e7f9a6b869476b41401460bd298f1d104376debe
Mikhail Saburov <saburov1985@gmail.com>
2024-04-23 23:30:57 +1100

    верстка

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-import Image from 'next/image';
+import { Gallery } from '@/components/gallery';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main>
+      <Gallery />
+    </main>
   );
 }
--- components/gallery.jsx
+++ components/gallery.jsx
@@ -2,11 +2,11 @@ import { Profile } from '@/components/profile';
 
 export const Gallery = () => {
   return (
-    <div>
+    <section className="flex gap-2">
       <h1>Список замечательный ученых</h1>
       <Profile />
       <Profile />
       <Profile />
-    </div>
+    </section>
   );
 };
