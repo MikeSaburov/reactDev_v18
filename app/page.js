28fcac4abab1ea4a66da98966adaec2d6a575262
Mikhail Saburov <saburov1985@gmail.com>
2024-04-23 23:06:34 +1100

    добавить изображение

--- app/page.js
+++ app/page.js
@@ -3,7 +3,12 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+      <Image
+        src={'https://i.imgur.com/MK3eW3Am.jpg'}
+        alt="Кэтрин Джонсон"
+        width={150}
+        height={150}
+      />
     </main>
   );
 }
