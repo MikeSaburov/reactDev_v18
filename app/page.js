d6d9492623cc06d40dfe5292e69497134f1112d4
Mikhail Saburov <saburov1985@gmail.com>
2024-04-24 22:54:19 +1100

    добавить пропсы

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
-import { Gallery } from '@/components/gallery';
+import { Profile } from '@/components/profile';
 
 export default function Home() {
   return (
-    <main>
-      <Gallery />
+    <main className="h-full">
+      <Profile
+        person={{ name: 'Lin Lanying', imageId: '1bX5QH6' }}
+        size={300}
+      />
     </main>
   );
 }
--- components/profile.jsx
+++ components/profile.jsx
@@ -1,13 +1,14 @@
 'use client';
 import Image from 'next/image';
 
-export const Profile = () => {
+export const Profile = ({ person, size = '150' }) => {
   return (
     <Image
       src={'https://i.imgur.com/MK3eW3Am.jpg'}
       alt="Кэтрин Джонсон"
-      width={150}
-      height={150}
+      width={size}
+      height={size}
+      className="rounded-3xl p-3"
     />
   );
 };
